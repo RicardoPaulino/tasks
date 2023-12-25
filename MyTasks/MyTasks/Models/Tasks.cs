a2205efe5b6f1c1aa448b77986c3c0ec5e2fd9f0
Ricardo Paulino do Nascimento <ricardo.paulino.nascimento@gmail.com>
2023-12-25 18:54:49 -0300

    add .vs and Web.config to gitignore

--- MyTasks/MyTasks/Models/Tasks.cs
+++ MyTasks/MyTasks/Models/Tasks.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace MyTasks.Models
 {
-    public class Tasks
+    public class Task
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
     }
 }
