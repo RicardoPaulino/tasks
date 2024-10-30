309ad715f59ce9bbbfd8ed0499b369477b0d877f
Ricardo Paulino <ricardo.paulino@byteon.com.br>
2024-10-29 22:31:06 -0300

    create component header

---
+++ frontend/task_vue_v1/app/src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">Header</header>
+</template>
+<script>
+export default {
+  name: "TheHeader",
+
+}
+</script>
+
+
+<style>
+  .header {
+    background: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #fff;
+}
+</style>
